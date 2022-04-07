e8960a6659b4ee0e7921d60825fee005d884e5f4
christawi <chhris502110@gmail.com>
2022-04-07 14:57:46 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+/**
+ * main - print Programming is like building a multilingual puzzle
+ * Return: 0 if exited correctly
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
